2ff5195e96f7c710f62f8cf0ac5ca081c1854aa9
Daniel <daniel.rendox@gmail.com>
2024-02-06 23:10:23 +0200

    Rename HabitStatusComputerTest to HabitStatusComputerImplTest

--- core/domain/src/test/java/com/rendox/routinetracker/core/domain/completion_history/HabitStatusComputerImplTest.kt
+++ core/domain/src/test/java/com/rendox/routinetracker/core/domain/completion_history/HabitStatusComputerImplTest.kt
@@ -16,7 +16,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.MethodSource
 
-class HabitStatusComputerTest {
+class HabitStatusComputerImplTest {
     private val defaultSchedule = Schedule.WeeklyScheduleByDueDaysOfWeek(
         dueDaysOfWeek = listOf(
             DayOfWeek.WEDNESDAY,
