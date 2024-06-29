470d5610f0b2b7f372e2f26f73d97de201f84848
Daniel <daniel.rendox@gmail.com>
2024-06-29 14:07:08 +0300

    Add unit tests for new db queries

---
+++ core/database/src/test/java/com/rendox/routinetracker/core/database/habit/CompletionHistoryLocalDataSourceImplTest.kt
@@ -0,0 +1,119 @@
+package com.rendox.routinetracker.core.database.habit
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import com.google.common.truth.Truth.assertThat
+import com.rendox.routinetracker.core.database.RoutineTrackerDatabase
+import com.rendox.routinetracker.core.database.completion_history.CompletionHistoryLocalDataSource
+import com.rendox.routinetracker.core.database.completion_history.CompletionHistoryLocalDataSourceImpl
+import com.rendox.routinetracker.core.database.di.localDataSourceModule
+import com.rendox.routinetracker.core.logic.time.rangeTo
+import com.rendox.routinetracker.core.model.Habit
+import com.rendox.routinetracker.core.model.Schedule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.LocalDate
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+import org.koin.test.get
+
+class CompletionHistoryLocalDataSourceImplTest : KoinTest {
+    private lateinit var sqlDriver: SqlDriver
+    private lateinit var completionHistoryLocalDataSource: CompletionHistoryLocalDataSource
+
+    private val testModule = module {
+        single {
+            sqlDriver
+        }
+    }
+
+    private val habit = Habit.YesNoHabit(
+        id = 1L,
+        name = "Test habit",
+        schedule = Schedule.EveryDaySchedule(
+            startDate = LocalDate(2024, 1, 1),
+        )
+    )
+    private val testCompletionHistory = listOf(
+        Habit.YesNoHabit.CompletionRecord(
+            date = LocalDate(2024, 1, 1),
+            numOfTimesCompleted = 1F,
+        ),
+        Habit.YesNoHabit.CompletionRecord(
+            date = LocalDate(2024, 3, 2),
+            numOfTimesCompleted = 1F,
+        ),
+        Habit.YesNoHabit.CompletionRecord(
+            date = LocalDate(2024, 5, 3),
+            numOfTimesCompleted = 1F,
+        ),
+    )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @BeforeEach
+    fun setUp() = runTest {
+        startKoin {
+            modules(
+                localDataSourceModule,
+                testModule,
+            )
+        }
+
+        sqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        RoutineTrackerDatabase.Schema.create(sqlDriver)
+
+        completionHistoryLocalDataSource = CompletionHistoryLocalDataSourceImpl(
+            db = get(), ioDispatcher = UnconfinedTestDispatcher()
+        )
+    }
+
+    @AfterEach
+    fun tearDown() {
+        stopKoin()
+    }
+
+    @Test
+    fun `assert getRecordsInPeriod returns only records within the given period`() = runTest {
+        completionHistoryLocalDataSource.insertCompletions(
+            completions = mapOf(1L to testCompletionHistory)
+        )
+        val records = completionHistoryLocalDataSource.getRecordsInPeriod(
+            habit = habit,
+            minDate = LocalDate(2024, 2, 1),
+            maxDate = LocalDate(2024, 8, 1),
+        )
+        assertThat(records).containsExactly(
+            testCompletionHistory[1],
+            testCompletionHistory[2],
+        )
+    }
+
+    @Test
+    fun `assert getMultiHabitRecords returns only records within the given period`() = runTest {
+        completionHistoryLocalDataSource.insertCompletions(
+            completions = mapOf(
+                1L to testCompletionHistory,
+                2L to testCompletionHistory,
+                3L to testCompletionHistory,
+            )
+        )
+        val habitsToPeriods = listOf(
+            listOf(habit, habit.copy(id = 2)) to LocalDate(2024, 2, 1)..LocalDate(2024, 8, 1),
+            listOf(habit.copy(id = 3)) to LocalDate(2024, 5, 3)..LocalDate(2024, 5, 3),
+            listOf(habit.copy(id = 4)) to LocalDate(2024, 1, 1)..LocalDate(2024, 1, 1),
+        )
+        val records = completionHistoryLocalDataSource.getMultiHabitRecords(habitsToPeriods)
+        val expectedRecords = mapOf(
+            1L to testCompletionHistory.takeLast(2),
+            2L to testCompletionHistory.takeLast(2),
+            3L to testCompletionHistory.takeLast(1),
+        )
+        assertThat(records).containsExactlyEntriesIn(expectedRecords)
+    }
+}
--- core/database/src/test/java/com/rendox/routinetracker/core/database/habit/VacationLocalDataSourceImplTest.kt
+++ core/database/src/test/java/com/rendox/routinetracker/core/database/habit/VacationLocalDataSourceImplTest.kt
@@ -7,6 +7,7 @@ import com.rendox.routinetracker.core.database.RoutineTrackerDatabase
 import com.rendox.routinetracker.core.database.di.localDataSourceModule
 import com.rendox.routinetracker.core.database.vacation.VacationLocalDataSource
 import com.rendox.routinetracker.core.database.vacation.VacationLocalDataSourceImpl
+import com.rendox.routinetracker.core.logic.time.rangeTo
 import com.rendox.routinetracker.core.model.Vacation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
@@ -15,6 +16,7 @@ import kotlinx.datetime.LocalDate
 import kotlinx.datetime.toLocalDate
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.koin.core.context.startKoin
@@ -72,7 +74,7 @@ class VacationLocalDataSourceImplTest : KoinTest {
         "2024-05-01, 2024-05-02, false, false",
         "2024-01-01, 2025-01-01, true, false",
     )
-    fun testGetVacationsInPeriodCompletedVacation(
+    fun testGetVacationsInPeriod(
         minDate: String,
         maxDate: String,
         shouldContain: Boolean,
@@ -99,5 +101,50 @@ class VacationLocalDataSourceImplTest : KoinTest {
         } else {
             assertThat(resultingVacations).isEmpty()
         }
+    }
+
+    @Test
+    fun testGetMultiHabitVacations() = runTest {
+        val habitIdsToVacations = mapOf(
+            1L to listOf(
+                Vacation(
+                    id = 1L,
+                    startDate = LocalDate(2024, 6, 8),
+                    endDate = LocalDate(2024, 7, 1),
+                ),
+                Vacation(
+                    id = 2L,
+                    startDate = LocalDate(2024, 8, 1),
+                    endDate = LocalDate(2024, 8, 2),
+                ),
+            ),
+            2L to listOf(
+                Vacation(
+                    id = 3L,
+                    startDate = LocalDate(2024, 10, 1),
+                    endDate = null,
+                )
+            ),
+            3L to emptyList(),
+            4L to listOf(
+                Vacation(
+                    id = 4L,
+                    startDate = LocalDate(2024, 6, 3),
+                    endDate = LocalDate(2024, 7, 2),
+                )
+            ),
+        )
+        vacationLocalDataSource.insertVacations(habitIdsToVacations = habitIdsToVacations)
+        val vacations = vacationLocalDataSource.getMultiHabitVacations(
+            habitsToPeriods = listOf(
+                listOf(1L, 2L) to LocalDate(2024, 6, 1)..LocalDate(2024, 7, 1),
+                listOf(3L, 4L) to LocalDate(2024, 6, 3)..LocalDate(2024, 6, 3),
+            )
+        )
+        val expectedResult = mapOf(
+            1L to habitIdsToVacations.getValue(1L).take(1),
+            4L to habitIdsToVacations.getValue(4L),
+        )
+        assertThat(vacations).containsExactlyEntriesIn(expectedResult)
     }
 }
