2297541e378b55fb47427355aa42da5ff7db70c3
Danylo Biliaiev <daniel.rendox@gmail.com>
2024-07-20 22:16:56 +0300

    Switch back to DatabasePrepopulatorNoOp

--- core/data/src/debug/java/com/rendox/routinetracker/core/data/di/DatabasePrepopulatorModule.kt
+++ core/data/src/debug/java/com/rendox/routinetracker/core/data/di/DatabasePrepopulatorModule.kt
@@ -1,15 +1,11 @@
 package com.rendox.routinetracker.core.data.di
 
 import com.rendox.routinetracker.core.data.DatabasePrepopulator
-import com.rendox.routinetracker.core.data.DatabasePrepopulatorRandom
+import com.rendox.routinetracker.core.data.DatabasePrepopulatorNoOp
 import org.koin.dsl.module
 
 val databasePrepopulatorModule = module {
     single<DatabasePrepopulator> {
-        DatabasePrepopulatorRandom(
-            habitLocalDataSource = get(),
-            completionHistoryLocalDataSource = get(),
-            vacationLocalDataSource = get(),
-        )
+        DatabasePrepopulatorNoOp()
     }
 }
